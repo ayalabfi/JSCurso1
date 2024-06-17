58f38f68d7d0e3e9ea05757be5db4a103e51e7d5
Ayala <ayalaborbafilho@gmail.com>
2024-06-17 17:12:04 -0300

    Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}KM/H!`);
+// Se possuir apenas o if, a condição é denominada simples
+ if (vel > 60){
+    console.log("Você ultrapassou a velocidade permitida!");
+}
+console.log("Utilize o cinto de segurança!");
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "França";
+console.log(`Vivendo em ${pais}`)
+// Quando além do if, possui um else na mesma condição, é uma condição composta.
+if (pais == "Brasil") {
+    console.log("Você é Brasileiro")
+} else {
+    console.log("Você é Estrangeiro")
+}
