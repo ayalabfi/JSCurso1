a12fd5f50f64736a74fae82ba247bc3dceae458f
Ayala <ayalaborbafilho@gmail.com>
2024-06-21 16:36:45 -0300

    Aula 14

---
+++ aula14/ex015.js
@@ -0,0 +1,21 @@
+/*var c = 1
+do {
+    console.log(c)
+    c++
+}while (c <= 5)
+*/
+
+/*
+O for tem a mesma função que o while, funcionando da seguinte forma:
+for(x;y;z){
+}
+x -> Inicia a variável
+y -> Faz o teste lógico
+z -> Faz o incremento
+{} -> É o bloco que executa a função enquanto o teste lógico for verdadeiro (true)
+*/
+console.log("Vai começar...")
+for(var c= 1;c <= 5; c++) {
+console.log(c)
+}
+console.log("Fim!")
